78d9b3dc66e749ba19d66a405d69d715d62608cf
Kamlesh Kumar <kamlesh.kumar@kindlebit.com>
2019-06-19 14:47:33 +0530

    Changes

--- app/Project.php
+++ app/Project.php
@@ -3,8 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Project extends Model
 {
-    //
+	 public static function boot()
+	    {
+	       parent::boot();
+	       static::creating(function($model)
+	       {
+	            $user = auth()->user();
+		        if($user->hasRole(User::ADMIN_ROLE) == false || $user->hasRole(User::MANAGER_ROLE) == false) {
+		          $model->creator_id =  $user->id;
+		        }
+
+	       });
+	   }
+
+     public function scopeCurrentUser($query)
+     {
+        $user = auth()->user();
+
+        if($user->hasRole(User::ADMIN_ROLE) == false || $user->hasRole(User::MANAGER_ROLE) == false) {
+          return $query->where('creator_id', $user->id);
+        }
+
+        return $query;
+     }
 }
